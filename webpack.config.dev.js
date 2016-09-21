b7738ba572daf77a3c7149b47dadad19918c46bd
Alex Aragon <Alex.Aragon@kingcounty.gov>
2016-09-21 12:26:44 -0700

    Init

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,31 +2,41 @@ import webpack from 'webpack';
 import path from 'path';
 
 export default {
+  // Enables debug info
   debug: true,
+  // Used for debugging
   devtool: 'cheap-module-eval-source-map',
+  // false = Display package info
   noInfo: false,
+  // Define entry points for app
   entry: [
     'eventsource-polyfill', // necessary for hot reloading with IE
     'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
     path.resolve(__dirname, 'src/index')
   ],
+  // Could set to node; webpack used to setup for environment
   target: 'web',
+  //Where to set up the files
   output: {
     path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
     publicPath: '/',
     filename: 'bundle.js'
   },
+  // Where are code is
   devServer: {
     contentBase: path.resolve(__dirname, 'src')
   },
+  // Enhance webpack with plugins
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
   module: {
     loaders: [
+      //Use babel to compile JS
       {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
       {test: /(\.css)$/, loaders: ['style', 'css']},
+      // Last four files are for BootStrap
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
       {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
